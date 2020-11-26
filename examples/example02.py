03fe53f404d8f502495b25642145d7d3af257330
shurajan <alexander.bralnin@gmail.com>
2020-11-26 23:58:55 +0300

    refactor for still in progress

--- examples/example02.py
+++ examples/example02.py
@@ -21,10 +21,13 @@ data = pd.read_csv(path, index_col = 0,  parse_dates=['TRADEDATE'])
 pf1_data = pd.DataFrame(data, columns=['AFKS', 'APTK', 'LNZL', 'MAGEP', 'MRKS', 'PLZL', 'ROLO', 'SELG'])
 
 pf1 = portfolio(data=pf1_data, risk_free_rate=0.01, freq=252)
-pf1.mc(1000000)
+pf1.mc(100000)
 pf1.plot_portfolios()
 pf1.print_results()
-#
+msr1 = pf1.max_sharpe_ratio()
+print(np.round(msr1['x'],decimals = 4))
+pf1.efficient_portfolios()
+# #
 ef1 = efficient_frontier(data=pf1_data)
 msr1 = ef1.max_sharpe_ratio()
 mv1 = ef1.min_volatility()
--- pfo/portfolio/portfolio.py
+++ pfo/portfolio/portfolio.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import scipy.optimize as sco
+import numpy as np
 from pfo.portfolio.mc import mc_random_portfolios
 from pfo.market_data import clean_data
+from pfo.portfolio.valuations import pf_valuation, pf_volatility, pf_mean_returns
+from pfo.stocks.returns import cov_matrix, mean_returns
 
 
 class portfolio(object):
     """Object that contains information about a investment portfolio.
     """
 
-    def __init__(self, data: pd.DataFrame, risk_free_rate=0.0425, freq=252):
+    def __init__(self, data: pd.DataFrame, weights = None, risk_free_rate=0.0425, freq=252):
         if not isinstance(freq, int):
             raise ValueError('Frequency must be an integer')
         elif freq <= 0:
@@ -36,6 +39,15 @@ class portfolio(object):
         self._max_sharpe_port = None
         self._max_sortino_port = None
         self._df_results = None
+
+        #####################
+        if weights is None:
+           self._weights = np.array([1./len(self._data.columns) for i in range(len(self._data.columns))])
+        else:
+            self._weights = np.array(weights)
+
+        self._cvm = cov_matrix(self._data)
+        self._mr = mean_returns(self._data, freq=self._freq)
 
 
     @property
@@ -92,7 +104,7 @@ class portfolio(object):
     #                                     SIMULATION                              #
     ###############################################################################
     def mc(self, num_portfolios=10000):
-        self._portfolios = mc_random_portfolios(data=self._data, risk_free_rate=self._risk_free_rate, \
+        self._portfolios = mc_random_portfolios(data=self._data, risk_free_rate=self._risk_free_rate,
                                                 num_portfolios=num_portfolios, freq=self._freq)
 
         self._min_vol_port = self._portfolios.iloc[self._portfolios['Volatility'].idxmin()]
@@ -108,3 +120,93 @@ class portfolio(object):
             [self._min_vol_port, self._min_downside_vol_port, self._max_sharpe_port, self._max_sortino_port],
             keys=['Min Volatiity', 'Down. Volatility', 'Max Sharpe Ratio', 'Max Sortino Ratio'], join='inner', axis=1)
 
+
+    ###############################################################################
+    #                                     EFFICIENT FRONTIER                      #
+    ###############################################################################
+
+    def max_sharpe_ratio(self):
+        num_stocks = len(self._data.columns)
+        #args = (self._data, self._risk_free_rate, self._freq)
+        constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+        bound = (0.0, 1.0)
+        bounds = tuple(bound for asset in range(num_stocks))
+
+        #returns, risk_free_rate, volatility
+
+        result = sco.minimize(self._neg_sharp_ratio, num_stocks * [1. / num_stocks, ], args=(),
+                              method='SLSQP', bounds=bounds, constraints=constraints)
+        return result
+
+    def min_volatility(self):
+        num_stocks = len(self._data.columns)
+        cvm = cov_matrix(self._data)
+        args = (cvm, self._freq)
+        constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+        bound = (0.0, 1.0)
+        bounds = tuple(bound for asset in range(num_stocks))
+
+        result = sco.minimize(pf_volatility, num_stocks * [1. / num_stocks, ], args=args,
+                              method='SLSQP', bounds=bounds, constraints=constraints)
+
+        return result
+
+    def efficient_return(self, target):
+        num_stocks = len(self._data.columns)
+        cvm = cov_matrix(self._data)
+
+        args = (cvm, self._freq)
+
+        def _pf_return(weights):
+            ret = pf_mean_returns(weights, self._mr)
+            return ret
+
+        constraints = ({'type': 'eq', 'fun': lambda x: _pf_return(x) - target},
+                       {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+        bounds = tuple((0, 1) for asset in range(num_stocks))
+        result = sco.minimize(pf_volatility, num_stocks * [1. / num_stocks, ], args=args,
+                              method='SLSQP', bounds=bounds, constraints=constraints)
+        return result
+
+    def efficient_frontier(self, returns_range):
+        efficients = []
+        for ret in returns_range:
+            efficients.append(self.efficient_return(ret))
+        return efficients
+
+    def efficient_portfolios(self, plot = True):
+        max_sharpe = self.max_sharpe_ratio()
+        min_vol = self.min_volatility()
+
+        res_max = pf_valuation(weights=max_sharpe['x'], data=self._data,
+                               risk_free_rate=self._risk_free_rate, freq=self._freq)
+
+        returns_max = res_max.get('Returns')
+        volatility_max = res_max.get('Volatility')
+
+        res_min = pf_valuation(weights=min_vol['x'], data=self._data,
+                               risk_free_rate=self._risk_free_rate, freq=self._freq)
+
+        returns_min = res_min.get('Returns')
+        volatility_min = res_min.get('Volatility')
+
+        target = np.linspace(returns_min, returns_max, 50)
+        self._efficient_portfolios = self.efficient_frontier(returns_range=target)
+
+        if plot:
+            plt.plot([p['fun'] for p in self._efficient_portfolios], target, 'k-x',
+                     label='efficient frontier', color = 'g')
+
+        return self._efficient_portfolios
+
+    def sharp_ratio(self, weights):
+        ret= pf_mean_returns(weights, self._mr)
+        vol = pf_volatility(weights, self._cvm, freq=self._freq)
+        return (ret - self._risk_free_rate)/vol
+
+    def _neg_sharp_ratio(self, weights):
+        return -self.sharp_ratio(weights)
+
+    def _neg_sortino_ratio(self, weights, data, risk_free_rate, freq=252):
+        sharp = pf_valuation(weights=weights, data=data, risk_free_rate=risk_free_rate, freq=freq).get('Sortino')
+        return -sharp
